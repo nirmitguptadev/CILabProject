d775f48cbed905c8fb3032aceb343e4e2498b1eb
Nirmit <nirmitgupta777@gmail.com>
2026-01-31 17:02:41 +0530

    Add initial unit test for App class

---
+++ src/test/java/com/muj/ci/AppTest.java
@@ -0,0 +1,10 @@
+package com.muj.ci;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
